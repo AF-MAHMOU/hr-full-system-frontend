34c82ac551f5ede7cfc40edfdf9c492366b448a1
MostafaSolim <mostafasol@gmail.com>
2025-12-07 09:07:23 +0200

    basic navigation

--- app/modules/time-management/page.tsx
+++ app/modules/time-management/page.tsx
@@ -10,21 +10,49 @@ import Link from 'next/link';
 
 export default function TimeManagementPage() {
     const pathname = usePathname();
-    const href = `${pathname}/shifts`;
+
+    const attendanceCorrectionHref = `${pathname}/attendanceCorrection`;
+    const attendanceRecordHref = `${pathname}/attendance`;
+    const holidayHref = `${pathname}/holiday`;
+    const latenessHref = `${pathname}/lateness`;
+    const notificationHref = `${pathname}/notification`;
+    const overtimeHref = `${pathname}/overtime`;
+    const scheduleRuleHref = `${pathname}/schedule`;
+    const shiftHref = `${pathname}/shifts`;
+    const timeExceptionHref = `${pathname}/timeException`;
+    
     return (
     <div className={s.container}>
         <div className={s.glow}></div>
         <div className={s.glow} style={{ top: "200px", right: "-100px", background: "radial-gradient(circle, rgba(165, 255, 80, 0.2), transparent 70%)" }}></div>
 
-        <Link href={href} className={s.button}>
+        <Link href={attendanceCorrectionHref} className={s.button}>
+            Attendance Correction
+        </Link>
+        <Link href={attendanceRecordHref} className={s.button}>
+            Attendance Record
+        </Link>
+        <Link href={holidayHref} className={s.button}>
+            Holidays
+        </Link>
+        <Link href={latenessHref} className={s.button}>
+            Lateness
+        </Link>
+        <Link href={notificationHref} className={s.button}>
+            Notifications
+        </Link>
+        <Link href={overtimeHref} className={s.button}>
+            Overtime
+        </Link>
+        <Link href={scheduleRuleHref} className={s.button}>
+            Schedule
+        </Link>
+        <Link href={shiftHref} className={s.button}>
             Shifts
         </Link>
-
-
-
-
-
-        
+        <Link href={timeExceptionHref} className={s.button}>
+            Time Exception
+        </Link>        
     </div>
     
 
